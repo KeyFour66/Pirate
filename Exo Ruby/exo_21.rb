2063b42ee81fb46ebbb99830507ca924e0cd6eed
Laurent Fourquet <fourquet@live.fr>
2020-04-07 13:04:17 +0200

    wip 22 pyramid ok faire le 17BG

--- Exo Ruby/exo_21.rb
+++ Exo Ruby/exo_21.rb
@@ -1,1 +1,21 @@
-exo_21.rb
+puts "Donne moi un chiffre entre 1 et 25 ? "
+print '>'
+n = gets.chomp.to_i
+
+#while n >= 1
+      #puts "* " * n
+      #n = n - 1
+    #end
+#while n <= 25
+  #puts ("* " * n).rjust(10)
+  #n += 1
+#end
+
+def pyramid(height)
+  height.times {|n|
+    print ' ' * (height - n)
+    puts '*' * ( n + 1)
+  }
+end
+
+pyramid (n)
