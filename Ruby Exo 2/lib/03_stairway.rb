5d16718cbba7454dc7d60bcaae67f5906705d6c7
Laurent Fourquet <fourquet@live.fr>
2020-04-08 16:46:01 +0200

    still wip stairs

--- Ruby Exo 2/lib/03_stairway.rb
+++ Ruby Exo 2/lib/03_stairway.rb
@@ -2,9 +2,26 @@ puts 'Bienvenue au 6éme sans ascenseur ! '
 
 score = rand(1..6)
 puts score
-
+stair = nil
 #
 case score
+
+when score == 5
+puts 'Bingo , tu avances 1 marche !'
+stair = stair +1
+
+when score == 6
+puts 'Bingo , tu avances 1 marche !'
+stair = stair +1
+
+when score == 1
+  stair = stair - 1
+  puts 'Dommage , tu descends 1 marche !'
+
+else
+  puts "Un coup dans l'eau !"
+  score = score
+end
 
 
 
