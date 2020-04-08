fb0e9a2f492f2fee1a14b424af675eda83c6b814
Laurent Fourquet <fourquet@live.fr>
2020-04-08 20:34:56 +0200

    pb pour boucler ...

--- Ruby Exo 2/lib/03_stairway.rb
+++ Ruby Exo 2/lib/03_stairway.rb
@@ -1,9 +1,10 @@
 puts 'Bienvenue au 6éme sans ascenseur ! '
-
+puts "lance les dés"
 score = rand(1..6)
 puts score
 stair = 0
-#
+attempt = 1
+
 case score
 
 when 5
@@ -22,5 +23,12 @@ else
   puts "Un coup dans l'eau !"
 end
 
+until score >= 10
+puts 'lance encore le dé' if stair =< 10
+puts 'félicitation ,te voila enfin en haut sans ascenseur !'
 
+puts "gagné en #{attempt} tentatives"
+  attempt = attempt +1
+  break if score >= 10
 
+end
