c9952e1aa1dac283ab5599fa7208fdbc6659c285
Laurent Fourquet <fourquet@live.fr>
2020-04-09 00:30:11 +0200

    miss 50 shortest

--- Day3Ruby/lib/00_journalists.rb
+++ Day3Ruby/lib/00_journalists.rb
@@ -5,23 +5,24 @@ puts a[1]
 #Combien y a-t-il de journalistes dans ce array ?
 puts " il y a #{a.length} journalistes "
 
-#a.count {|i| [Aude].include? i }
-
 #Combien d'handle contiennent un numéro ?
-num = a.count {|e| e =~ /^@[A-Z]/}
+num = a.count {|e| e =~ /[0-9]/}
 puts num
 #Combien d'handle contiennent les 4 lettres du prénom "Aude" à la suite (sans prendre en compte les majuscules) ?
-
+aude = a.count {|e| e =~ /aude/}
+puts aude
 #Combien commencent par une majuscule (première lettre juste après le @) ?
 maj = a.count {|e| e =~ /^@[A-Z]/}
 puts maj
 #Combien contiennent au moins une majuscule ?
-
+majmin = a.count {|e| e =~ /[A-Z]/}
+puts majmin
 #Combien y a-t-il de underscore _ dans tous les pseudos confondus ?
-
+under = a.count {|e| e =~ /_/}
+puts under
 #Trie la liste de handle par ordre alphabétique.
-#sorted_a = a.sort
-#puts sorted_a
+sorted_a = a.sort
+puts sorted_a
 
 #Quels sont les 50 handles les plus courts de ce array ?
 
